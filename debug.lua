49210ea39c3f12a5d62b04c9c0009977d576a0c8
Shawn Shojaie <shawn.shojaie@gmail.com>
2020-04-18 23:54:11 -0700

    Add next attempt at implementation of debugger loop
    Use a pseudo game loop instead of wrapping callbacks with coroutines
    and yields. This approach doesn't work because upon a normal return of
    _update or _draw yields control back to the process calling coresume.

--- debug.lua
+++ debug.lua
@@ -6,12 +6,35 @@ function _debug_conf(...)
  for callback in all(t_varargs) do
   local wrapped = _debug_wrap(callback)
   add(r_callbacks, wrapped)
-  menuitem(#r_callbacks+1, "", function() wrapped() end)
-  printh(#r_callbacks, "space_invaders")
-  _set_trace()
+  menuitem(#r_callbacks+1, "conf function "..tostr(#r_callbacks), function() wrapped() end)
  end
  menuitem(1, "_stop", function() stop() end)
  return debug.unpack(r_callbacks)
+end
+
+-- a simplistic game loop to replace PICO-8s and support coroutine resumption
+function _debug_game_loop(update, draw, init)
+ local debug_init = function()
+  init()
+  while true do
+   update()
+   draw()
+   flip()
+  end
+ end
+ return debug_init
+end
+
+function _debug_wrap(func)
+ local inner = function()
+  local c = cocreate(func)
+  local active, err = coresume(c)
+  while active do
+   extcmd("pause")
+   active, err = coresume(c)
+  end
+ end
+ return inner
 end
 
 function _unpack(t)
@@ -24,32 +47,8 @@ function _unpack(t)
  end
 end
 
-function _debug_wrap(func)
- local inner = function()
-  local c = cocreate(func)
-  local active, err = coresume(c)
-  while active do
-   active, err = coresume(c)
-  end
- end
- return inner
-end
-
-function _debug_menu()
- 
- menuitem(2, "_shutdown", function() extcmd("shutdown") end)
- menuitem(3, "_shutdown", function() extcmd("shutdown") end)
- menuitem(4, "_shutdown", function() extcmd("shutdown") end)
- menuitem(5, "_shutdown", function() extcmd("shutdown") end)
-end
-
-function _set_trace()
- extcmd("pause")
- yield()
-end
-
 -- as follows is the public API
 debug = {}
-debug.set_trace = _set_trace
-debug.conf = _debug_conf
+debug.set_trace = yield
 debug.unpack = _unpack
+debug.replace_game_loop = _debug_game_loop
